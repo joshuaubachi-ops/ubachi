a021b452a55bf2ea3f3365e12530a3cdef77c333
joshuaubachi-ops <joshuaubachi@gmail.com>
2025-12-02 22:37:58 +0100

    Add files via upload

---
+++ physics.py
@@ -0,0 +1,13 @@
+print("..........welcome to formula chooser..........")
+print("1. formula A\n2. formula B\n3. formula C\n4. formula D")
+choice = input("choose a formula from (1-4)")
+if choice == "1":
+    print("your now using formula A")
+elif choice == "2":
+    print("your now using formula B")
+elif choice == "3":
+    print("your now using formula C")
+elif choice == "4":
+    print("your now using formula D")
+else:
+    print("invalid choice pls try again")
